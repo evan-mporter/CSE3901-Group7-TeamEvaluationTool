8824203cf9456c2b8ac4568f6dd93674c9512834
Kaiyuan Zheng <zheng.1760@osu.edu>
2022-04-13 12:18:25 -0400

    some structure changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+    has_many :groups
+    #Is this should be feedbackItems or feedbackitems or feedback_items?
+    has_many :feedback_items
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
