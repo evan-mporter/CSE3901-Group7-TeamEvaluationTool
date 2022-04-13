6da601e353b688406fafc0b2679d66b9de617b1c
Kaiyuan Zheng <zheng.1760@osu.edu>
2022-04-12 21:12:55 -0400

    first version of scaffold student with only constrains on model. Other components are original.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_12_195917) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
