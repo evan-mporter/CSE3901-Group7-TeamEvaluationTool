915c1b55372d53a0f73b4e67e6065ac95f258866
Evan <evan_porter4@yahoo.com>
2022-04-12 23:31:26 -0400

    Force generate scaffold that I forgot about and drop old instructor table, error on my part but should be fixed now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_022112) do
+ActiveRecord::Schema.define(version: 2022_04_13_032802) do
 
   create_table "instructors", force: :cascade do |t|
     t.string "name"
