386ecab4f52b64f0295833d80c2e31d6965c9550
Adrian Vovk <adrianvovk@gmail.com>
2022-04-12 23:47:56 -0400

    Add minimal implementation of ApplicationController
    Missing lots of funcitonality that depends on some routes existing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  resources :instructors
-  resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Instructor routes
+  resources :instructors
+
+  # Student routes
+  resources :students
+
+  # Group routes
+
+  # Evaluations routes
+
+  # Application routes
+  root 'application#root'
+  get '/login', to: 'application#login'
+
+  # Handle test reset, to make testing the app on Heroku possible
+  get '/full-system-reset-for-testing-only', to: 'application#full_reset'
+  post '/full-system-reset-for-testing-only', to: 'application#do_full_reset'
 end
