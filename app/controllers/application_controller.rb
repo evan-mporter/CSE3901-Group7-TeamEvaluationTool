ec50f94620000ba4ccd3ab1ff3dc3d3e441d5f02
Adrian Vovk <adrianvovk@gmail.com>
2022-04-17 00:36:38 -0400

    Set up root redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
   include LoginHelper
+
   def root
-    render html: "TODO!"
+    # Must be a new instance! There are no instructors. Prompt to make one
+    if Instructor.count.zero?
+      redirect_to new_instructor_url
+
+    # An instructor is logged in. Redirect to their homepage
+    elsif is_inst_logged_in?
+      redirect_to instructor_url(current_inst)
+
+    # A student is logged in. Redirect to their homepage
+    elsif is_student_logged_in?
+      redirect_to student_url(current_student)
+
+    # Nobody is logged in. Redirect to the login page
+    else
+      redirect_to login_url
+    end
   end
 
   def login
