56b1f5a72bf7de56b3379b04020eb6a7a15bf87a
Adrian Vovk <adrianvovk@gmail.com>
2022-04-21 03:20:49 -0400

    Tweak home nav treatment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,13 @@ module ApplicationHelper
   # Helps construct the navbar
   def nav_item(name, icon, controller, path, method="get")
 
+    # Highlight the active nav item, with special treatment of "home"
     active = (params[:controller] == controller)
     if (inst_logged_in? and (request.fullpath == instructor_path(current_inst))) or
        (student_logged_in? and (request.fullpath == student_path(current_student)))
       active = (controller == "home")
+    elsif not logged_in? and (controller == "home")
+      active = true
     end
     active = active ? "active" : ""
 
