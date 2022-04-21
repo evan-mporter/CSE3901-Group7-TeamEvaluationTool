b539c7121a5ce5c23a1e7ee902e75c8fdbd9ad38
Adrian Vovk <adrianvovk@gmail.com>
2022-04-21 03:10:42 -0400

    Fixup navbar bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,16 @@
 module ApplicationHelper
 
-  def nav_item name, icon, controller, path, method="get"
-    child = "<i class=\"fa fa-#{icon}\" aria-hidden=\"true\"></i> #{name}".html_safe
+  # Helps construct the navbar
+  def nav_item(name, icon, controller, path, method="get")
 
-    if controller == "home"
-      if inst_logged_in?
-        active = (request.fullpath == instructor_path(current_inst))
-      elsif student_logged_in?
-        active = (request.fullpath == student_path(current_student))
-      else
-        active = (params[:controller] == "application")
-      end
-    else
-      active = (params[:controller] == controller)
+    active = (params[:controller] == controller)
+    if (inst_logged_in? and (request.fullpath == instructor_path(current_inst))) or
+       (student_logged_in? and (request.fullpath == student_path(current_student)))
+      active = (controller == "home")
     end
     active = active ? "active" : ""
-    
+
+    child = "<i class=\"fa fa-#{icon}\" aria-hidden=\"true\"></i> #{name}".html_safe
     link_to child, path, class: "nav-item nav-link text-center px-3 #{active}", method: method
   end
 
